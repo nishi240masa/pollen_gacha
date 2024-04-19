71fbbf75b356b4ea22623f9f9ef5b6404b71e033
watagassa <k23095kk@aitech.ac.jp>
2024-04-19 14:40:01 +0900

    実際の画面に花粉量表示

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Kahun } from "./components/Kahun";
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <header className="App-header">
+        <Kahun></Kahun>
+      </header>
     </div>
   );
 }
-
 export default App;
