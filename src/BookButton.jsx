31024bcba8dd418e0a4ba68442afa36449348e59
tanaka0005 <tanaka00005.com>
2024-04-19 14:51:01 +0900

    delete

--- src/BookButton.jsx
+++ src/BookButton.jsx
@@ -17,37 +17,8 @@ export const BookButton = (props) => {
   
     return <h1 style={BookButton}>{children}</h1>;
 
-  //   const LevelButton = {
-  //     position:'fixed',  /*ポジションを固定*/
-  //     display:'flex',
-  //     textAlign:'center',
-  //     transform:'scale(3)',     /*ボタンを5倍に*/
-  //     left:'80%',
-  //     top:'70px',
-
-  //   };
-  //   return <h1 style={LevelButton}>{children}</h1>;
+  
    };
   
 
-//    import { useState } from 'react';
 
-// import {BookButton} from "./BookButton.jsx";
-// import { LevelButton } from "./LevelButton.jsx";
-// import { NameButton } from "./NameButton.jsx";
-
-// export default function ButtonArea () {
-//   const [showMore, setShowMore] = useState(false);
-
-//   function handleClick ()  {
-//     setShowMore(!showMore);
-//   }
-//   return(
-//     <div>
-//         <BookButton><button onClick = {handleClick}>図鑑</button></BookButton>
-//         <LevelButton><button>レベル</button></LevelButton>
-//         <NameButton><button>名前</button></NameButton>
-//     </div>
-//   );
-    
-// }
